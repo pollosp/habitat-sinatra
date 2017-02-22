4467fa6972bff0d6bf5d5d434dbabc93a4c83241
pollosp <pollo.es.pollo@gmail.com>
2017-02-22 11:38:00 +0100

    using rack and trying to use toml config

--- sinatra-pkg/plan.sh
+++ sinatra-pkg/plan.sh
@@ -1,19 +1,18 @@
 pkg_name=sinatra
 pkg_origin=pollosp
-pkg_version=0.0.1
+pkg_version=0.0.2
 pkg_maintainer="Omar"
 pkg_license=('MIT')
 pkg_source=https://www.hiszpanskinaluzie.pl/myapp.tar.gz
-pkg_shasum=1d015e655e1b0d0467da5f299824b8231b5a8c0920a45f121d620be73d1c2890
+pkg_shasum=a991e21e6559937473a0ff7da42d7efd35e3949d868331a43570962add71d042
 pkg_deps=(core/ruby)
 pkg_build_deps=(core/patch core/make core/gcc)
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
-pkg_exports=(
-  [port]=serverport
-)
+pkg_exports=( [port]=listening_port )
+pkg_exposes=(port)
 
-pkg_svc_run="ruby /app/myapp.rb"
+pkg_svc_run="rackup -o 0.0.0.0 -p $pkg_exposes /app/config.ru"
 pkg_svc_user="root"
 pkg_interpreters=(bin/ruby)
 
